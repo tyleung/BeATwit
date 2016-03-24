89c95e91ed5e8aded65ba7602385cc3c19aa10f8
tyleung <tyleung@ualberta.ca>
2016-03-24 02:57:31 -0600

    conversion to direct messages in progress

--- main.py
+++ main.py
@@ -14,34 +14,49 @@ filter_track = '@450bot'
 game_id = 0
 host_player = ""
 player_list = []
+game = None
 
 class TweetListener(tweepy.StreamListener):
 	join_str = "join game " + str(game_id)
 	
 	def on_data(self, data):
+		global host_player
+		global game
 		# Twitter returns data in JSON format - we need to decode it first
 		decoded = json.loads(data)
 		
 		# Also, we convert UTF-8 to ASCII ignoring all bad characters sent by users
 		user = decoded['user']
 		text = decoded['text'].encode('ascii', 'ignore')
+		player = Player(user)
+		screenname = decoded['user']['screen_name']
 		
 		# Strip the "@450bot " beginning of the tweet
 		text = text[len(filter_track) + 1:]
 		if text == "create" and len(player_list) == 0:
 			self.new_game()
-			self.join_game(user)
-			host_player = user
+			self.join_game(player)
+			host_player = player.screen_name
+			print(player.screen_name)
 		elif text == join_str:
-			self.join_game(user)
-		
+			self.join_game(player)
+		elif text == "start":
+			if player.screen_name == host_player:
+				game = playGame()
+				api.update_status( "(Game #" + str(game_id) + ") @" + screenname + "you started the game, make your first move!")
+			else:
+				api.update_status("@" + screenname + "you are not allowed to start this game")
+		elif text == "shoot" or text == "reload" or text =="defend" or text == "Bang":
+			game.get_reply(user,text)
+				
 		return True
     
 	def on_error(self, status):
 		print status
 		
-	def join_game(self, user):
-		player = Player(user)
+		
+	def join_game(self, player):
+		#player = Player(user)
 		for p in player_list:
 			if p.screen_name == player.screen_name:
 				print(player.screen_name + " has already joined the game.")
@@ -57,6 +72,76 @@ class TweetListener(tweepy.StreamListener):
 		global join_str
 		join_str = "join game " + str(game_id)
 		api.update_status("(Game #" + str(game_id) + ") A new game has been created. Tweet '@450bot " + join_str + "' to join the game.")
+		
+		
+		
+class playGame:
+	
+	count_reply = 0
+	gameover_list =[]
+	player_num = 0
+	def __init__(self):
+		player_num = len(player_list)
+		
+	def get_reply(self,who,reply):
+		self.count_reply+=1
+		#check if the player replied
+		player_list[index(who)].reply = 1
+		#0:reload, 1:shoot,2:defend,3:bang
+		
+		
+		if reply == "shoot":
+			player_list[index(who)].move = 1
+		elif reply == "reload":
+			player_list[index(who)].move = 0
+			player_list[index(who)].qi += 1
+		elif reply == "defend":
+			player_list[index(who)].move = 2
+		elif reply == "Bang":
+			player_list[index(who)].move = 3
+		
+		if count_reply == player_num:
+			result = get_result()
+		
+		
+	def get_result(self):
+		for player in player_list:
+			if player.move == 3:
+				for player2 in player_list:
+					if player2.move != 3:
+						gameover_list.append(player)
+				sn = player.screen_name
+				m = "@%s Wow! looks like you killed somebody. Nice move!"%(sn)
+				api.update_status(m,player.id)
+
+			elif player.move == 1:
+				for player2 in player_list:
+					if player2.move == 0:
+						gameover_list.append(player)
+				sn = player.screen_name
+				m = "@%s Wow! looks like you killed somebody. Nice move!"%(sn)
+				api.update_status(m,player.id)
+			
+		for player in gameover_list:
+			sn = player.screen_name
+			m = "@%s I'm sorry,you've eliminated in this round..."%(sn)
+			api.update_status(m,player.id)
+			player_list.remove(player)
+		
+		for player in player_list:
+			player.move = 0
+		
+		
+		gameover_list = []
+		count_reply = 0
+		
+		if len(player_list) == 1:
+			player = player_list[0]
+			sn = player.screen_name
+			m = "@%s Nicely done! You are the winner of this round!"%(sn)
+			api.update_status(m,player.id)
+			
+	
 	
 def main():
 	l = TweetListener()
